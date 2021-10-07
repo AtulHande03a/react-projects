151812b04334cae0012af3062820f549a5120308
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-08 01:22:34 +0530

    updated tours

--- 02-tour/src/Tours.js
+++ 02-tour/src/Tours.js
@@ -2,12 +2,15 @@ import React from "react";
 import Tour from "./Tour";
 const Tours = ({ tours }) => {
   return (
-    <section className="title">
-      <h2>our tours</h2>
-      <div className="underline">
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
-        })}
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"> </div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour} />;
+          })}
+        </div>
       </div>
     </section>
   );
