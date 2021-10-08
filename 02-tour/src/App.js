e54d4dfe0f9f4db45707a52e4ab9bd8a0eff4ee0
Atul Kumar Hande <handeatul03a@gmail.com>
2021-10-08 19:54:10 +0530

    added tours project funtinality

--- 02-tour/src/App.js
+++ 02-tour/src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour= (id)=>{
+    const newTours= tours.filter((tour)=> tour.id !== id)
+    setTours(newTours)
+  }
+
   const fetchTours = async () => {
     setIsLoading(true);
     try {
@@ -35,9 +40,23 @@ function App() {
     );
   }
 
+  if(tours.length ===0){
+    return (
+      <main>
+      <div className='title'>
+      <h2>no tours left</h2>
+      <button className='btn' onClick={fetchTours}>refresh</button>
+      </div>
+      </main>
+    )
+    
+  }
+
+
+
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
